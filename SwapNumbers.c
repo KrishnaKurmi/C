70777f2339e6074b79a88f9c5cca65b04428cbc0
Krishna Kurmi <hollybuddy.ts2206@gmail.com>
2025-06-25 19:45:54 +0530

    Update SwapNumbers.c

--- SwapNumbers.c
+++ SwapNumbers.c
@@ -1,23 +1,17 @@
 #include<stdio.h>
 //Swapping using third variable
-void swapUsingVar(int a, int b){
-    int c=a;
-    a=b;
-    b=c;
-    printf("A = %d \nB = %d\n",a,b);
+void swapUsingVar(int *a, int *b){
+    int c = *a;
+    *a = *b;
+    *b = c;
 }
 int main(){
     int a = 10,b = 20;
     printf("Before Swapping \n");
     printf("A = %d \nB = %d\n",a,b);
     printf("After Swapping \n");
-    swapUsingVar(a,b);
+    swapUsingVar(&a,&b);
+    printf("A = %d \nB = %d\n",a,b);
     printf("------------------\n");
-
-    //Swapping without using third variable
-    printf("Before Swapping \n");
-    printf("A = %d \nB = %d\n",a,b);
-    a=a+b-(b=a);
-    printf("After Swapping \n");
-    printf("A = %d \nB = %d\n",a,b);
+    return 0;
 }
